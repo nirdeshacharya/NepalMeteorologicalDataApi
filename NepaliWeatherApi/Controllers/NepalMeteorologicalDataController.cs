d262c38b8f34a2b0c375857fea5eff33a6a1f0f4
Nirdesh Acharya <nirdesh.acharya@hotmail.com>
2018-12-21 22:42:12 +0545

    initial

---
+++ NepaliWeatherApi/Controllers/NepalMeteorologicalDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NepaliWeatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NepalMeteorologicalDataController : Controller
+    {
+        private readonly WeatherService _wetherServ;
+        public NepalMeteorologicalDataController(WeatherService weatherService)
+        {
+            _wetherServ = weatherService;
+        }
+        [HttpGet]
+        public object WeatherApi(string city=null)
+        {
+            if (city != null)
+            {
+              return  _wetherServ.GetWeatherByDetail(city);
+            }
+            else
+            {
+                return _wetherServ.GetAllWeather();
+            }
+        }
+        
+
+    }
+}
---
+++ NepaliWeatherApi/WeatherService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NepaliWeatherApi
+{
+
+    public class WetherModel
+    {
+        public string City { get; set; }
+        public decimal MinTemp { get; set; }
+        public decimal MaxTemp { get; set; }
+        public decimal Rainfall { get; set; }
+    }
+    public class WeatherService
+    {
+       public WetherModel GetWeatherByDetail(string city)
+        {
+            return ParseWeather().Where(x => x.City == city).FirstOrDefault();
+        }
+        public List<WetherModel> GetAllWeather()
+        {
+            return ParseWeather().ToList();
+        }
+        public List<WetherModel> ParseWeather()
+        {
+            WebClient webClient = new WebClient();
+            string page = webClient.DownloadString("http://www.mfd.gov.np/weather/");
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(page);
+
+            var table = doc.DocumentNode.SelectSingleNode("//table[@class='table']")
+                        .Descendants("tr")
+                        .Skip(1)
+                        .Where(tr => tr.Elements("td").Count() > 1)
+                        .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                        .ToList();
+            var result = new List<WetherModel>();
+           foreach(var item in table)
+            {
+               
+                result.Add(new WetherModel
+                {
+                    City = item[0].ToLower(),
+                    MaxTemp = Convert.ToDecimal(item[1]),
+                    MinTemp = Convert.ToDecimal(item[2]),
+                    Rainfall = Convert.ToDecimal(item[3].Replace("*","")),
+                });
+            }
+            return result;
+            
+        }
+    }
+        
+}
